a942933b87f4b178f6048dea3b22cff04e46ef97
EricZ5 <65284562+EricZ5@users.noreply.github.com>
2020-06-10 20:16:01 +0200

    Add files via upload

---
+++ 11_Javascript_DOM/01 DOM/01DOM.js
@@ -0,0 +1,14 @@
+let mainHeading = document.getElementsByTagName("h1");//document => index.html
+
+console.log(mainHeading);
+
+let mainParagraph = document.getElementsByTagName("p");
+
+console.log(mainParagraph);
+
+/**
+ * AUFGABE
+ * 1) Defintion einer Variable, die über getElementsByTagName auf das p-Tag zugreift
+ * 2) Zugriff auf Eigenschaft innerText
+ * 3) Ausgabe in der Konsole
+ */
